8a0d733b606f5a2727912d985759560c2f7da34a
sidiquimk <masoodsdq1@gmail.com>
2018-12-08 12:51:53 -0500

    second comit

--- src/test/java/MavenTestGitHub/MavenTestGitHub/IgnoreThis.java
+++ src/test/java/MavenTestGitHub/MavenTestGitHub/IgnoreThis.java
@@ -4,7 +4,13 @@ public class IgnoreThis {
 
 	public static void main(String[] args) {
 		System.out.println("ignore this class");
+		
+		hello();
 
+	}
+	
+	public static void hello(){
+		System.out.println("this is hello method");
 	}
 
 }
